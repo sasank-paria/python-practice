2039b42fb52e681ca15c2c4c562fbc18d35db1de
sasank-paria <pariatapan0@gmail.com>
2022-03-07 17:16:20 +0530

    new file

---
+++ even or odd.py
@@ -0,0 +1,8 @@
+num1=int(input(("enter a number to check even or odd:")))
+
+print("enter number is :",num1)
+
+if num1%2==0 :
+    print("it is a even number")
+else:
+    print("it is a odd number")
